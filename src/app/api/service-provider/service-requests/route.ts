1785fcf6e59207072260c5ef8e988eaf752623b7
Nicolas Trejos <129107686+3J0sB@users.noreply.github.com>
2025-05-16 03:36:41 -0400

    moved the solicitudes recientes to requests page 😼👍

---
+++ src/app/api/service-provider/service-requests/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+export async function GET(request: NextRequest) {
+    try {
+        const serviceRequests = await prisma.serviceRequest.findMany({
+            include:{
+                user: {
+                    select: {
+                        id: true,
+                        name: true,
+                        lastName: true,
+                        email: true,
+                    }
+                },
+                service: {
+                    select: {
+                        id: true,
+                        title: true,
+                        description: true,
+                        price: true,
+                    }
+                }
+            }
+        })
+
+
+        if (!serviceRequests || serviceRequests.length === 0) {
+            return NextResponse.json({ error: "No se encontraron solicitudes de servicio" }, { status: 404 });
+        }
+        return NextResponse.json(serviceRequests, { status: 200 });
+    } catch (error) {
+        
+    }
+}
