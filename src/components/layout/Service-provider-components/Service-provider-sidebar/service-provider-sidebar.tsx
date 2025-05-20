c6ed0bb3a23d59b10904d4d171abe881ba949df8
Nicolas Trejos <129107686+3J0sB@users.noreply.github.com>
2025-05-20 00:28:30 -0400

    updated service-provider  sidebar  😼👍

--- src/components/layout/Service-provider-components/Service-provider-sidebar/service-provider-sidebar.tsx
+++ src/components/layout/Service-provider-components/Service-provider-sidebar/service-provider-sidebar.tsx
@@ -103,25 +103,7 @@ function ServiceProviderSidebar({ userName, userType, userLastName }: SidebarPro
             collapsed={collapsed}
           />
 
-          <SidebarLink
-            href="/service-provider/calendar"
-            icon={
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-              </svg>
-            }
-            text="Calendario"
-            active={pathname.startsWith("/serviceprovider/calendar")}
-            collapsed={collapsed}
-          />
 
-          {!collapsed && (
-            <div className="pt-6">
-              <div className="px-4 py-2 text-xs font-semibold text-gray-500 uppercase tracking-wider">
-                Ajustes
-              </div>
-            </div>
-          )}
 
           <SidebarLink
             href="/service-provider/profile"
